8458d9f36e31e85c8bc96e7092981ebfd63c2766
V I Key <brydo@outlook.com>
2026-09-20 23:14:11 +0100

    Fix syntax error in middleware.js

--- functions/xmasletters/_middleware.js
+++ functions/xmasletters/_middleware.js
@@ -26,7 +26,7 @@ function letterHTML(id, content, footerAudioUrl) {
 
   
 
-  i    if (!content || !content.text) {
+  if (!content || !content.text) {
 
   var footerAudio = '';
   if (footerAudioUrl) {
